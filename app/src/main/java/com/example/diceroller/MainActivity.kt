2529347b0e127cdfe6af338b8ec2a05b96f49a33
int3ll3kt <int3ll3kt@gmail.com>
2022-05-11 18:20:05 +0100

    Completed Coding Challenge 7 Challenge: Add a second button to the app labeled "Count Up" that appears just below the Roll button. When tapped, the Count Up button should get the current value of the result text view, add 1 to it, and update the text view. Make sure you handle these edge cases:
    If the result text view does not yet contain a number (that is, if the text view still has the default "Hello World" string), set the result text to 1.
    If the number is already 6, do nothing.
    Changing Text with the click of a Button using the onclick listener.
    
    1 Warning: A variable is assigned to itself. I look forware to a better approach in the future.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+//import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,16 +12,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        // Get the Button view from the layout and assign a click listener to each
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
+
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { increment() }
     }
 
+    //Click listener action for Roll Button
     private fun rollDice(){
 
         val randomInt: Int = (1..6).random()
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         val resultText: TextView = findViewById(R.id.result_test)
         resultText.text = randomInt.toString()
+
+    }
+    //Click listener action for Count Up Button
+    private fun increment(){
+        val resultText: TextView = findViewById(R.id.result_test)
+
+        //Check if Result TextView is at default, if yes covert to 1
+        if (resultText.text.toString() == "Hello World!"){
+            resultText.text = "1"
+        }
+
+        //Otherwise increment Result TextView value by 1 otherwise if value is 6, let it be
+        else {
+        var resultInt: Int = resultText.text.toString().toInt()
+            when (resultInt){
+                1, 2, 3, 4, 5 -> resultInt += 1
+                6 -> resultInt = resultInt
+            }
+            resultText.text = resultInt.toString()
+
+        }
     }
 }
