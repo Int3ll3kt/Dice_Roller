df129bcf492029d3249954d8be580ee5dc0d5923
int3ll3kt <int3ll3kt@gmail.com>
2022-05-15 22:49:29 +0100

    Android Kotlin Fundamentals 1.3 Image Resource and Compatibility Challenge: Modify the DiceRoller app to have two dice. When the user taps the Roll button, each dice should have a value independent of the other.
    Challenge Status: Completed

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,47 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-//import android.widget.Toast
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var diceImage: ImageView
+    private lateinit var diceImage2: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         //Initialize diceImage by attaching it to a view
-
         diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image_2)
 
-        // Get the Button view from the layout and assign a click listener to each
+        // Get the Button view from the layout and assign a click listener action
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-
-        val countUpButton: Button = findViewById(R.id.count_up_button)
-        countUpButton.setOnClickListener { increment() }
-
-        //Get the Button view from the layout and set an onClick listener action
-        val resetButton: Button = findViewById(R.id.reset_button)
-        resetButton.setOnClickListener{reset()}
     }
 
-    //Click listener action for Reset Button
-    private fun reset() {
-
-        val resultText = findViewById<TextView>((R.id.result_test))
-        resultText.text = "0"
-    }
 
     //Click listener action for Roll Button
     private fun rollDice(){
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage(): Int{
 
         //Randomly generate a number for each dice roll
-        val randomInt: Int = (1..6).random()
-
-        //Update variable drawableResource with a dice icon equivalent to number generated
-        val drawableResource = when (randomInt){
+        //Return diceImage int equivalent to randomInt
+        return when ((1..6).random()){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -52,31 +45,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             6 -> R.drawable.dice_6
             else -> R.drawable.ic_vr_avatar
-        }
-        //Update ImageView with a dice icon equivalent to the random Integer generated.
-        diceImage.setImageResource(drawableResource)
-        val resultText: TextView = findViewById(R.id.result_test)
-        resultText.text = randomInt.toString()
-
-    }
-    //Click listener action for Count Up Button
-    private fun increment(){
-        val resultText: TextView = findViewById(R.id.result_test)
-
-        //Check if Result TextView is at default, if yes covert to 1
-        if (resultText.text.toString() == "Let's Start!"){
-            resultText.text = "1"
-        }
-
-        //Otherwise increment Result TextView value by 1 otherwise if value is 6, let it be
-        else {
-        var resultInt: Int = resultText.text.toString().toInt()
-            when (resultInt){
-                1, 2, 3, 4, 5 -> resultInt += 1
-                6 -> resultInt = resultInt
-            }
-            resultText.text = resultInt.toString()
-
         }
     }
 }
